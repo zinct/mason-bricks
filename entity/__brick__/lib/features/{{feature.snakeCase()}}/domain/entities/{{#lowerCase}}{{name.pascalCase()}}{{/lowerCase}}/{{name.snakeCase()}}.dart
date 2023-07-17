64a45bcdac320ab6fc722a0b09178488bafebf1a
Indra Mahesa <indramahesa128@gmail.com>
2023-07-17 23:26:41 +0700

    fix: change entities to entity

---
+++ entity/__brick__/lib/features/{{feature.snakeCase()}}/domain/entities/{{#lowerCase}}{{name.pascalCase()}}{{/lowerCase}}/{{name.snakeCase()}}.dart
@@ -0,0 +1,21 @@
+///
+/// {{name.snakeCase()}}_entity.dart
+/// lib/features/{{feature.snakeCase()}}/domain/entities
+/// 
+/// Created by Indra Mahesa https://github.com/zinct
+/// 
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '{{name.snakeCase()}}.g.dart';
+part '{{name.snakeCase()}}.freezed.dart';
+
+@freezed
+sealed class {{name.pascalCase()}}Entity with _${{name.pascalCase()}} {
+  const factory {{name.pascalCase()}}Entity({
+    int? id,
+  }) = _{{name.pascalCase()}}Entity;
+
+  factory {{name.pascalCase()}}Entity.fromJson(Map<String, dynamic> json) =>
+      _${{name.pascalCase()}}EntityFromJson(json);
+}
